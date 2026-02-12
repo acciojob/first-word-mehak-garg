fb28d9d6acdd5e06d4b756996be8ef25f07d20eb
mehak-garg <mehakgarg741@gmail.com>
2026-02-12 15:43:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function firstWord(s) {
   // your code here
-}
+	if(s.length===0)
+	{
+		return "";
+	}
+	
+	for(let i=0;i<s.length;i++)
+		{
+if(s[i]==" ")
+{
+	break;
+}}
+			if(i===s.length)
+			{
+				return s.slice(0);
+			}
+return s.slice(0,i);
 
+
+}			
 // Do not change the code below
 
 const s = prompt("Enter String:");
